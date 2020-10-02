25bbff023d4af0e125a2b81ead82788fb8e0564b
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-02 15:43:20 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="text-black red-bg-100 bg-green-700">
-    <h1>Mert</h1>
+    <div className="body">
+
+      <div className="header bg-black h-40 flex">
+        
+        <div className="logo-container">
+          <img className="h-full pl-20" src="/images/real-white-logo.png"></img>
+        </div>
+
+        <div className="navbar-container flex text-white items-center justify-end">
+          <div className="links-container">
+            <ul className="links-ul flex">
+              <li>Menu</li>
+              <li>Order</li>
+              <li>About</li>
+              <li>Shop</li>
+            </ul>            
+          </div>
+
+          <div className="icons-container">
+            <ul className="icons-ul flex">
+              <li>Insta</li>
+              <li>Yelp</li>
+              <li>Face</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
