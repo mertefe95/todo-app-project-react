7a5cd5d39ec4b55f5b97fadb717facb458bed455
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-05 12:13:21 +0300

    menu-added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Nav from "../src/components/Nav";
 import Home from "../src/components/Home";
 import Footer from "../src/components/Footer";
+import Menu from "../src/components/Menu";
 
 function App() {
   return (
     <div>
       <Nav/>
-      <Home/>
+      <Menu/>
       <Footer/>
     </div>
     
---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Zoom from 'react-reveal/Zoom';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import TailwindCSS from "../assets/tailwind.css";
+import MenuImage1 from "../assets/images/menu-image-1.jpg";
+import MenuImage2 from "../assets/images/menu-image-2.jpg";
+import MenuImage3 from "../assets/images/cafemenu.jpg";
+
+
+function Menu() {
+return(
+<div class="menu-page-container">
+    
+</div>
+);
+}
+
+export default Menu;
