4ecea2471244f00449b040666a186a77760ae287
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-23 14:27:02 +0300

    content section is created

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TailwindCSS from "../assets/tailwind.css";
-import nyx from "../assets/images/nyx.png";
+import nyx from "../assets/images/bg-image.png";
 
 function Home() {
 return (
-<main className="home-main">
-  
+<main className="home-main h-screen">
+    
+        <div className="upper-content-above relative">
+            
+            <img src={nyx} className="h-full w-full" />
+            
+            <div className="upper-content-text uppercase text-center absolute top-0 text-white p-2 ">
+                <h2 className="text-4xl">Welcome to Nyx Bar & Cocktail</h2>
+                <h3 className="text-2xl">Your local kadikoy bar. No, not that one.</h3>
+                <h3 className="text-2xl">The other one, the one you love.</h3>
+                <h3 className="text-2xl">Focusing on all things crafted - from booze & brews to the</h3>
+                <h3 className="text-2xl">Finest fare.</h3>
+                <p className="text-base">Nyx Bar & Cocktail harks back to 2000's era. One of craftmanship and authenticity, of dimly lit encounters and great converrsations.</p>
+                <p className="text-base">Our well-informed barkeeps pour local craft beers, hand-drawn ales, a selection of our very favorite wines, a wide range of spirits and liquors, and carefully crafted</p>
+                <p className="text-base">cocktails.</p>
+                <p className="text-base">As for food, only the best seasonal and local ingredients are found under our roof - served simply and deliciously.</p>
+            </div>
+
+            <div className="upper-content-below absolute left-0 bottom-0">
+            <ul className="upper-content-below-ul">
+                <li>
+                    <h4 className="uppercase">Location</h4>
+                    <p> 
+                        Kozyatagi District, Kayasultan Street.
+                    <br/>
+                        No:11/3 Istanbul/Turkey
+                    </p>
+                </li>
+                <li>
+                    <h4 className="uppercase">Hours</h4>
+                    <p>
+                        Updated hours on Instagram <a href="#">@xxxx</a>
+                    </p>     
+                </li>
+                <li>
+                    <h4 className="uppercase">Contact</h4>
+                    <p>
+                        info@nyxbar.com
+                        <a href="#">(212) 300-4924</a>
+                    </p>
+                </li>
+            </ul>
+            </div>
+
+        </div>
+
+    
 </main>
 );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import Zoom from 'react-reveal/Zoom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab, faFacebookF, faInstagram, faYelp, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBullhorn, faHome, faImages, faWarehouse } from '@fortawesome/free-solid-svg-icons';
+import { faBarcode, faBars, faBullhorn, faHome, faImages, faWarehouse } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fab)
 
@@ -12,26 +12,45 @@ function Nav() {
 return(
 <header className="header">
 
-    <nav className="nav flex justify-around items-center text-white uppercase h-40 pt-20">
-        <div className="logo-div">
-            <h2 className="mr-64">NYX BAR & COCKTAIL</h2>
-        </div>
-    
-        <div className="links-icons-div flex">
-            <ul className="menu-links flex">
-                <li className="px-2"><a href="/">Home</a></li>
-                <li className="px-2"><a href="/about">About</a></li>
-                <li className="px-2"><a href="#">Contact</a></li>
-                <li className="px-2"><a href="#">Projects</a></li>
+    <nav className="nav text-white uppercase h-40 pt-20">
+
+            <span class="navbar-toggle" id="js-navbar-toggle">
+                <FontAwesomeIcon icon={faBars} />
+            </span>
+
+            <a href="#" class="logo"><h2 className="mr-64">NYX BAR & COCKTAIL</h2></a>
+        
+            <ul className="menu-links main-nav">
+                <li className="">
+                    <a href="/">Home</a>
+                </li>
+                <li className="">
+                    <a href="/about">About</a>
+                </li>
+                <li className="">
+                    <a href="#">Contact</a>
+                </li>
+                <li className="">
+                    <a href="#">Projects</a>
+                </li>
             </ul>
 
-            <ul className="icons-links flex">
-                <li className="px-2"><a href="#"><FontAwesomeIcon icon={faFacebookF} /></a></li>
-                <li className="px-2"><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
-                <li className="px-2"><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
-                <li className="px-2"><a href="#"><FontAwesomeIcon icon={faBullhorn} /></a></li>
+            <ul className="icons-links main-nav-2">
+                <li className="">       
+                    <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+                </li>
+                <li className="">
+                    <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+                </li>
+                <li className="">
+                    <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+                </li>
+                <li className="">
+                    <a href="#"><FontAwesomeIcon icon={faBullhorn} /></a>
+                </li>
             </ul>
-        </div>
+
+        
     </nav>
 </header>
 );
