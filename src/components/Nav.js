2b211ab54003f893f4e6e0a230d911e93761d812
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-24 18:22:10 +0300

    adjusted css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,13 +12,13 @@ function Nav() {
 return(
 <header className="header">
 
-<nav className="nav text-white uppercase h-40 pt-20 flex flex-col justify-center items-center h-full">
+<nav className="nav text-white uppercase h-40 pt-20 sm:pt-56  flex flex-col justify-centerh-full">
 
         <div className="logo-div">
             <a href="#" class="logo"><h2 className="">NYX BAR & COCKTAIL</h2></a>
         </div>
     
-        <ul className="menu-links main-nav">
+        <ul className="menu-links main-nav  text-center md:text-4xl ">
             <li className="py-2">
                 <a href="/">Home</a>
             </li>
@@ -33,7 +33,7 @@ return(
             </li>
         </ul>
 
-        <ul className="icons-links main-nav-2 mt-18">
+        <ul className="icons-links main-nav-2 mt-18  text-center md:text-4xl">
             <li className="py-1">       
                 <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
             </li>
--- tailwind.js
+++ tailwind.js
@@ -10,7 +10,7 @@ module.exports = {
   separator: ':',
   theme: {
     screens: {
-      sm: '600',
+      sm: '600px',
       md: '832px',
       lg: '1024px',
       xl: '1280px',
