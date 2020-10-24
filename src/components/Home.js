361b44af2da1939f1181543c017ecbea8694fd9a
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-24 17:46:29 +0300

    responsive homepage nyx welcome sectionn added

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,59 +2,28 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TailwindCSS from "../assets/tailwind.css";
 import nyx from "../assets/images/bg-image.png";
+import cocktailsImg from "../assets/images/cocktails-1.jpg";
+import mainDrinksImg from "../assets/images/main-drinks.jpg";
 
 function Home() {
 return (
-<main className="home-main h-screen">
-    
-        <div className="upper-content-above relative">
-            
-            <img src={nyx} className="h-full w-full" />
-            
-            <div className="upper-content-text text-center absolute top-0 left-0 flex flex-col w-full h-full text-white p-2 ">
-                <h2 className="text-4xl mt-20 uppercase">Welcome to Nyx Bar & Cocktail</h2>
-                <h3 className="text-2xl mt-8 tracking-widest uppercase">Your local kadikoy bar. No, not that one.</h3>
-                <h3 className="text-2xl tracking-widest uppercase">The other one, the one you love.</h3>
-                <h3 className="text-2xl tracking-widest uppercase">Focusing on all things crafted - from booze & brews to the</h3>
-                <h3 className="text-2xl tracking-widest uppercase">Finest fare.</h3>
-                <p className="text-base mt-10">Nyx Bar & Cocktail harks back to 2000's era. One of craftmanship and authenticity, of dimly lit encounters and great converrsations.</p>
-                <p className="text-base mt-6">Our well-informed barkeeps pour local craft beers, hand-drawn ales, a selection of our very favorite wines, a wide range of spirits and liquors, and carefully crafted</p>
-                <p className="text-base">cocktails.</p>
-                <p className="text-base mt-6">As for food, only the best seasonal and local ingredients are found under our roof - served simply and deliciously.</p>
+<main className="main">
+    <div className="first-content-div relative flex justify-center items-center">
+        <img src={nyx} className="first-bg-img " />
+        <div className="first-div-text absolute top-0 text-center mt-8 mx-8">
+            <h1 className="font-bold text-xl xl:mt-24 lg:mt-40 xl:text-5xl xl:mb-4">WELCOME TO NYX</h1>
+            <p className="uppercase text-base mt-4 xl:text-3xl xl:mb-6"><strong>
+                Your local kadikoy moda bar. No, not that one. <br/>The other one, the one you love.<br/>
+                Focusing on all things crafted - from booze & brews to the <br/> finest fare.       
+            </strong></p>
 
-                <hr className="hr-div mt-8 w-4/5 ml-40 border-none bg-white"/>
-            </div>
-
-            
-            <div className="upper-content-below  absolute bottom-0 text-center w-full text-white ">
-            <ul className="upper-content-below-ul flex flex-row justify-evenly items-center mb-24">
-                <li>
-                    <h4 className="uppercase font-bold mb-4">Location</h4>
-                    <p> 
-                        Kozyatagi District, Kayasultan Street.
-                    <br/>
-                        No:11/3 Istanbul/Turkey
-                    </p>
-                </li>
-                <li>
-                    <h4 className="uppercase font-bold mb-4">Hours</h4>
-                    <p>
-                        Updated hours on Instagram <a href="#">@xxxx</a>
-                    </p>     
-                </li>
-                <li>
-                    <h4 className="uppercase font-bold mb-4">Contact</h4>
-                    <p>
-                        info@nyxbar.com
-                        <a href="#">(212) 300-4924</a>
-                    </p>
-                </li>
-            </ul>
-            </div>
+            <p className="text-sm mt-4 xl:text-xl">Nyx Bar & Cocktails harks back to a bygone era. One of craftmanship and authenticity, of dimly lit encounters and great conversations.<br/>
+            Our well-informed barkeeps pour local craft beers, hand-drawn ales, a selection of our very favorite wines, a wide range of spirits and liquors, and carefully crafted<br/>
+            cocktails.<br/>As for food, only the best seasonal and local ingredients are found under our roof - served simply and deliciously.
+            </p>    
 
         </div>
-
-    
+    </div>
 </main>
 );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,49 +12,46 @@ function Nav() {
 return(
 <header className="header">
 
-    <nav className="nav text-white uppercase h-40 pt-20">
+<nav className="nav text-white uppercase h-40 pt-20 flex flex-col justify-center items-center h-full">
 
-            <span class="navbar-toggle" id="js-navbar-toggle">
-                <FontAwesomeIcon icon={faBars} />
-            </span>
+        <div className="logo-div">
+            <a href="#" class="logo"><h2 className="">NYX BAR & COCKTAIL</h2></a>
+        </div>
+    
+        <ul className="menu-links main-nav">
+            <li className="py-2">
+                <a href="/">Home</a>
+            </li>
+            <li className="py-2">
+                <a href="/about">About</a>
+            </li>
+            <li className="py-2">
+                <a href="#">Contact</a>
+            </li>
+            <li className="py-2 mb-1">
+                <a href="#">Projects</a>
+            </li>
+        </ul>
 
-            <a href="#" class="logo"><h2 className="mr-64">NYX BAR & COCKTAIL</h2></a>
-        
-            <ul className="menu-links main-nav">
-                <li className="">
-                    <a href="/">Home</a>
-                </li>
-                <li className="">
-                    <a href="/about">About</a>
-                </li>
-                <li className="">
-                    <a href="#">Contact</a>
-                </li>
-                <li className="">
-                    <a href="#">Projects</a>
-                </li>
-            </ul>
+        <ul className="icons-links main-nav-2 mt-18">
+            <li className="py-1">       
+                <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+            </li>
+            <li className="py-1">
+                <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+            </li>
+            <li className="py-1">
+                <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
+            </li>
+            <li className="py-1">
+                <a href="#"><FontAwesomeIcon icon={faBullhorn} /></a>
+            </li>
+        </ul>
 
-            <ul className="icons-links main-nav-2">
-                <li className="">       
-                    <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
-                </li>
-                <li className="">
-                    <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
-                </li>
-                <li className="">
-                    <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
-                </li>
-                <li className="">
-                    <a href="#"><FontAwesomeIcon icon={faBullhorn} /></a>
-                </li>
-            </ul>
-
-        
-    </nav>
+    
+</nav>
 </header>
 );
 }
-
 
 export default Nav;
